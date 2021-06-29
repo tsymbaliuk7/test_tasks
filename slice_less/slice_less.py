9f212bbe3bdd2f4de9acae13e9ffe377cbde5b1b
Dima Tsymbaliuk <58704936+tsymbaliuk7@users.noreply.github.com>
2021-06-29 15:19:50 +0300

    small fixes on first

--- slice_less/slice_less.py
+++ slice_less/slice_less.py
@@ -2,6 +2,5 @@ def slice_less(my_list: list, lesser: int):
     return list(sorted(filter(lambda x: x > lesser, my_list), reverse=True))
 
 
-if __name__ == '__main__':
-    lst = list(range(10, 20))
-    print(slice_less(lst, 15))
+lst = list(range(10, 20))
+print(slice_less(lst, 15))
