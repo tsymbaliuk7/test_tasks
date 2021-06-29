365d0b9fcc3823e187866d9077c983617d73a201
Dima Tsymbaliuk <58704936+tsymbaliuk7@users.noreply.github.com>
2021-06-29 15:17:50 +0300

    first and second task done

---
+++ print_numbers/print_numbers.py
@@ -0,0 +1,9 @@
+def print_numbers():
+    for i in range(11, 80):
+        string = ''
+        string = string + '$$' if i % 3 == 0 else string
+        string = string + '@@' if i % 5 == 0 else string
+        print(string if string else i)
+
+
+print_numbers()
